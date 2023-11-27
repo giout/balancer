266eac364861322b59c3b8089d6a058a730f9641
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-11-27 11:43:28 -0400

    performance data manipulation ops

--- src/utils/table.util.js
+++ src/utils/table.util.js
@@ -3,18 +3,6 @@ const fs = require('fs')
 const path = require('path')
 const filePath = path.join(__dirname, '../balancing/table.json')
 
-const fileToObj = () => {
-    // almacena el archivo en un objeto
-    const file = fs.readFileSync(filePath, 'utf-8')
-    return JSON.parse(file)
-}
-
-const objToFile = (obj) => {
-    // almacena el objeto en el archivo
-    const string = JSON.stringify(obj)
-    fs.writeFileSync(filePath, string, 'utf-8')
-}
-
 const setService = (name, data) => {
     const obj = fileToObj()
     obj[name] = data
@@ -40,6 +28,18 @@ const restartTable = () => {
         m2: { ram: 0, cpu: 0, processes: 0, time: 0, error: false },
         m3: { ram: 0, cpu: 0, processes: 0, time: 0, error: false }
     })
+}
+
+const fileToObj = () => {
+    // almacena el archivo en un objeto
+    const file = fs.readFileSync(filePath, 'utf-8')
+    return JSON.parse(file)
+}
+
+const objToFile = (obj) => {
+    // almacena el objeto en el archivo
+    const string = JSON.stringify(obj)
+    fs.writeFileSync(filePath, string, 'utf-8')
 }
 
 module.exports = {
