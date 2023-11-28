f4d8c5df058fa0d4e85de7df54e81e8412eaa58d
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-11-27 20:37:41 -0400

    changed json file manipulation for a javascript object manipulation (table)

---
+++ src/balancing/table.js
@@ -0,0 +1,17 @@
+const table = {
+  "m1": {
+    "ram": 0.3657646179199219,
+    "cpu": 2400,
+    "processes": 0,
+    "time": 4144
+  },
+  "m2": {
+    "ram": 0.3274650573730469,
+    "cpu": 2400,
+    "processes": 0,
+    "time": 3156
+  },
+  "m3": { "ram": 0.3761711120605469, "cpu": 2400, "processes": 0, "time": 4260 }
+}
+
+module.exports = table
--- src/utils/table.util.js
+++ src/utils/table.util.js
@@ -1,10 +1,7 @@
-// metodos para la manipulacion de table.json
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join(__dirname, '../balancing/table.json')
+// metodos para la manipulacion de table.js
+let table = require('../balancing/table.js')
 
 const chooseService = () => {
-    const table = fileToObj()
     const points = { m1: 0, m2: 0, m3: 0 }
 
     // se calculan los valores maximos/minimos
@@ -63,9 +60,7 @@ const chooseService = () => {
 }
 
 const setService = (name, data) => {
-    const table = fileToObj()
     table[name] = data
-    objToFile(table)
 }
 
 const getService = (name) => {
@@ -74,55 +69,28 @@ const getService = (name) => {
 }
 
 const addProcess = (name) => {
-    const table = fileToObj()
     table[name].processes++
-    setTable(table)
 }
 
 const removeProcess = (name) => {
-    const table = fileToObj()
     table[name].processes--
-    setTable(table)
 }
 
 const getProcesses = (name) => {
-    const table = fileToObj()
     return table[name].processes
 }
 
-const setTable = (data) => {
-    objToFile(data)
-}   
-
-const getTable = () => {
-    return fileToObj()
-}
-
 const restartTable = () => {
-    setTable({
+    table = {
         m1: { ram: 0, cpu: 0, processes: 0, time: 0 },
         m2: { ram: 0, cpu: 0, processes: 0, time: 0 },
         m3: { ram: 0, cpu: 0, processes: 0, time: 0 }
-    })
-}
-
-const fileToObj = () => {
-    // almacena el archivo en un objeto
-    const file = fs.readFileSync(filePath, 'utf-8')
-    return JSON.parse(file)
-}
-
-const objToFile = (obj) => {
-    // almacena el objeto en el archivo
-    const string = JSON.stringify(obj)
-    fs.writeFileSync(filePath, string, 'utf-8')
+    }
 }
 
 module.exports = {
     setService,
     getService,
-    setTable,
-    getTable,
     restartTable,
     addProcess,
     removeProcess,
